3ab42bf4a82a99cb6d4dcc8799cafcbb297341d2
CMonk <tianlang0704@gmail.com>
2020-09-21 00:42:59 +0800

    更新基础交互

--- Assets/UILogic.cs
+++ Assets/UILogic.cs
@@ -1,21 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+using System;
 
 public class UILogic : MonoBehaviour
 {
-    UIBase ui;
     // Start is called before the first frame update
     void Start()
     {
-        ui = GetComponent<UIBase>();
-        var dialog = ui.GetObj("Ex_弹窗");
-        dialog.SetActive(false);
+        this.i("Ex_弹窗").SetActive(false);
+        this.i<Button>("Ex_导游").onClick.AddListener(()=>{
+            ShowDialog("这是点了导游啊? 安逸哦!");
+        });
+        this.i<Button>("Ex_小僧").onClick.AddListener(()=>{
+            ShowDialog("这是一个小僧呢!");
+        });
+    }
+
+    void ShowDialog(string content, Action okAction = null, Action cancelAction = null) {
+        UnityAction unityOkAction = new UnityAction(()=>{
+            this.i("Ex_弹窗").SetActive(false);
+            if (okAction == null) return;
+            okAction();
+        });
+        UnityAction unityCancelAction = new UnityAction(()=>{
+            this.i("Ex_弹窗").SetActive(false);
+            if (cancelAction == null) return;
+            cancelAction();
+        });
+        this.i("Ex_弹窗").SetActive(true);
+        this.i("Ex_弹窗").i<TextMeshProUGUI>("Ex_弹窗内容").text = content;
+        this.i("Ex_弹窗").i<Button>("Ex_YES").onClick.AddListener(unityOkAction);
+        this.i("Ex_弹窗").i<Button>("Ex_No").onClick.AddListener(unityCancelAction);
+        this.i("Ex_弹窗").lc(UIBase.LifeCycle.OnDisable, () => {
+            this.i("Ex_弹窗").i<Button>("Ex_YES").onClick.RemoveListener(unityOkAction);
+            this.i("Ex_弹窗").i<Button>("Ex_YES").onClick.RemoveListener(unityCancelAction);
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
