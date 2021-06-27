ebc478544d6c69869c7d208341b3d9aa6ee0777e
CMonk <tianlang0704@gmail.com>
2021-06-28 01:42:52 +0800

    更新UI适配

--- Assets/Scripts/Runtime/Utils/UIManager.cs
+++ Assets/Scripts/Runtime/Utils/UIManager.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : SingletonBehaviour<UIManager>
 {
+    public Vector2 DesignResolution = new Vector2(750, 1334);
     public GameObject UIRoot;
+    public Canvas Canvas;
+    void Awake()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateResolution();
     }
-
+    private int lastScreenWidth;
+    private int lastScreenHeight;
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateResolution();
+    }
+
+    void UpdateResolution()
+    {
+        if (lastScreenWidth != Screen.width || lastScreenHeight != Screen.height) {
+            lastScreenWidth = Screen.width;
+            lastScreenHeight = Screen.height;
+            OnScreenSizeChanged();
+        }
+    }
+ 
+    void OnScreenSizeChanged()
+    {
+        var scaler = Canvas.GetComponent<CanvasScaler>();
+        scaler.referenceResolution = DesignResolution;
+        var designWHRatio = (float)DesignResolution.x / DesignResolution.y;
+        var screenWHRatio = (float)Screen.width / Screen.height;
+        if (screenWHRatio > designWHRatio) {
+            scaler.matchWidthOrHeight = 1;
+        } else {
+            scaler.matchWidthOrHeight = 0;
+        }
     }
 
     public void AddToRoot(GameObject go)
